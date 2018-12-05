aefec0590aae73691f91ad0a2d36bdd7beb8412d
lcjingdi <lcjingdi@gmail.com>
2018-12-05 08:55:06 +0800

    123

--- src/index.js
+++ src/index.js
@@ -10,40 +10,22 @@ import ReactDOM from 'react-dom' // 把创建好的组件和虚拟 DOM 放到展
 // const myh1 = React.createElement('h1', {id: 'myh1'}, '这是一个h1标签')
 // const myh1 = <h1 id="myh1">主题</h1>
 
-class WebSite extends React.Component {
-    constructor() {
-        super()
-
-        this.state = {
-            name: "翼课网",
-            site: "https://www.ekwing.com"
-        }
-    }
-    render() {
-        return (
-            <div>
-                <Name name={this.state.name}></Name>
-                <Link site={this.state.site}></Link>
-            </div>
-        )
-    }
-}
-class Name extends React.Component {
-    render() {
-        return (
-            <h1>{this.props.name}</h1>
-        )
-    }
-}
-class Link extends React.Component {
-    render() {
-        return (
-            <a href={this.props.site}>{this.props.site}</a>
-        )
-    }
-}
+const element = <h1>Hello</h1>
 
 // 3. 使用 ReactDOM 把虚拟 DOM 渲染到页面上 
 // 参数1：要渲染的虚拟 DOM
 // 参数2：指定页面上的 DOM 元素，当做容器
-ReactDOM.render(<WebSite />, document.getElementById('app'))
+ReactDOM.render(element, document.getElementById('root'))
+
+function tick() {
+    const element = (
+        <div>
+            <h1>hello</h1>
+            <h1>It is{new Date().toLocaleTimeString()}.</h1>
+            <p>123123123</p>
+        </div>
+    )
+    ReactDOM.render(element, document.getElementById('root'))
+}
+
+setInterval(tick, 1000);
